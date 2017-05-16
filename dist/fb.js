aabc9279232b7bc940a1d943e9b7631e9119c24f
byronhsu <byronhsu1230@gmail.com>
2017-05-16 23:22:36 +0800

    login can work

--- dist/fb.js
+++ dist/fb.js
@@ -1,50 +1,36 @@
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+      window.fbAsyncInit = () => {
+        window.FB.init({
+          appId      : '1945913712308994',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v2.9'
+        });
+        document.getElementById('hello').style.display='none';
+        window.FB.AppEvents.logPageView();
+        window.FB.Event.subscribe('auth.statusChange', statusChangeCallback);
+      };
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+  statusChangeCallback = (response) => {
     if (response.status === 'connected') {
-        console.log(document.getElementsByClassName('fb-login-button')[0]);
-        document.getElementsByClassName('fb-login-button')[0].style.display = 'none';
-        testAPI();
-        return 1;
+      didLogin();
     } else {
-        document.getElementById('hello').style.display = 'none';
+      didNotLogin();
     }
-}
-
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '1945913712308994',
-        cookie     : true, 
-        xfbml      : true, 
-        version    : 'v2.8' 
-    });
-    FB.getLoginStatus(function(response) {
-        if(statusChangeCallback(response)){
-            console.log('!!!');
-        }
-        
-    });
-    FB.Event.subscribe('auth.statusChange',()=>{
-        console.log('123123');
-    })
-};
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+  }
+didLogin = () => {
+    window.FB.api('/me', (res) => {
+        document.getElementsByClassName('fb-login-button')[0].style.display='none';
+        document.getElementById('hello').style.display='inline-block';
+        document.getElementById('hello').innerHTML=`Hello ${res.name}`;
     });
 }
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-        document.getElementById('hello').innerHTML=`Hello ${response.name}`
-    });
+didNotLogin = () => {
+        document.getElementsByClassName('fb-login-button')[0].style.display='inline-block';
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-  entry: './index.js',
+  entry: {
+    index:'./index.js',
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [{
